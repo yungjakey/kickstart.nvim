92bedf2cd0c47369a23be4d3ceb7fff45bb88a49
yungjakey <jakob.melchard@orf.at>
2025-07-31 07:04:06 +0200

    buffer close on q

--- lua/custom/config/autocmds.lua
+++ lua/custom/config/autocmds.lua
@@ -38,3 +38,34 @@ vim.api.nvim_create_autocmd({ 'VimLeave' }, {
     vim.fn.system { 'wezterm', 'cli', 'set-tab-title', '' }
   end,
 })
+
+-- Set up the autocommand to create the keymap for specific buffer types
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup('BufferKeymaps', { clear = true }),
+
+  callback = function(ctx)
+    local a = vim.api
+
+    -- Check if buffer meets criteria for the 'q' keymap
+    if
+      not vim.bo[ctx.buf].modifiable or vim.bo[ctx.buf].buftype == 'nofile'
+      -- Add more conditions here if needed:
+      -- or vim.bo[ctx.buf].buftype == 'help'
+      -- or vim.bo[ctx.buf].buftype == 'quickfix'
+      -- or vim.tbl_contains({'help', 'qf', 'man'}, vim.bo[ctx.buf].filetype)
+    then
+      -- Create buffer-local keymap for 'q'
+      vim.keymap.set('n', 'q', function()
+        if #a.nvim_list_wins() > 1 then
+          a.nvim_win_close(0, false) -- Close the window
+        else
+          vim.cmd.normal 'ga' -- Show character info under cursor if last window
+        end
+      end, {
+        buffer = ctx.buf,
+        desc = 'Quit (or close/hide) buffer',
+        silent = true,
+      })
+    end
+  end,
+})
